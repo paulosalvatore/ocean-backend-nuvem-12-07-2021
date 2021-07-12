72f7040ebd3cc96485d884642d0b23934931c555
Paulo Salvatore <paulo@salvatore.tech>
2021-07-12 10:55:50 -0300

    Conexão com banco de dados desabilitada

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const { MongoClient, ObjectId } = require("mongodb");
 
     console.info("Conectando ao banco de dados...");
 
-    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+    //const client = await MongoClient.connect(url, { useUnifiedTopology: true });
 
     console.info("MongoDB conectado com sucesso!");
 
-    const db = client.db(dbName);
+    //const db = client.db(dbName);
 
     const app = express();
 
@@ -43,7 +43,8 @@ const { MongoClient, ObjectId } = require("mongodb");
     const lista = ["Senhor dos Anéis", "Harry Potter"];
     //              0                   1
 
-    const filmes = db.collection("filmes");
+    //const filmes = db.collection("filmes");
+    const filmes = undefined;
 
     // [GET] - Read All
     app.get("/filmes", async (req, res) => {
